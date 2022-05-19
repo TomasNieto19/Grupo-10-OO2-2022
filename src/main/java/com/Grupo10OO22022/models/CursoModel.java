289e78dc40df2ce7c6ec310021a8efbd1ac37256
FranciscoArmendariz <francisco.armendariz123578951@gmail.com>
2022-05-19 10:54:40 -0300

    Modelos hechos y atributo activo en branch

---
+++ src/main/java/com/Grupo10OO22022/models/CursoModel.java
@@ -0,0 +1,34 @@
+package com.Grupo10OO22022.models;
+
+import java.util.Set;
+
+import com.Grupo10OO22022.entities.Aula;
+import com.Grupo10OO22022.entities.Espacio;
+import com.Grupo10OO22022.entities.Fecha;
+import com.Grupo10OO22022.entities.Materia;
+import com.Grupo10OO22022.entities.Profesor;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter @Setter @NoArgsConstructor
+public class CursoModel extends NotaPedidoModel {
+
+	
+	private Set<Fecha> fechas;
+	
+	private String comisión;
+
+	public CursoModel(char turno, Aula aula, int cantEstudiantes, Materia materia, String observaciones,
+			Set<Espacio> espaciosAsignados, Set<Profesor> profesores, boolean pendiente, Set<Fecha> fechas,
+			String comisión) {
+		super(turno, aula, cantEstudiantes, materia, observaciones, espaciosAsignados, profesores, pendiente);
+		this.fechas = fechas;
+		this.comisión = comisión;
+	}
+	
+	
+	
+}

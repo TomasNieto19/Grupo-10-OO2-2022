cb969a43763bb4db0ad6c7db234f0b5d0d27d5d2
Tomas Nieto <tnieto852@gmail.com>
2022-06-02 01:16:34 -0300

    Elimino OrderBy de la query

--- src/main/java/com/Grupo10OO22022/repositories/ICursoRepository.java
+++ src/main/java/com/Grupo10OO22022/repositories/ICursoRepository.java
@@ -9,15 +9,17 @@ import com.Grupo10OO22022.entities.Curso;
 @Repository("cursoRepository")
 public interface ICursoRepository extends JpaRepository<Curso, Integer> {
 
+	// Filtra por palabra clave todos estos campos
 	@Query("SELECT c FROM Curso c JOIN c.profesores p JOIN c.materia m JOIN c.aula a JOIN a.edificio e WHERE c.activo = true and "
-			+ "CONCAT (c.turno, c.cantEstudiantes, c.observaciones, c.comision, " + "p.nombre, p.apellido, "
+			+ "CONCAT (c.cantEstudiantes, c.observaciones, c.comision, " + "p.nombre, p.apellido, "
 			+ "m.materia, m.codMateria, " + "a.numero, " + "e.edificio) " + "LIKE %?1%")
 	public List<Curso> search(String keyword);
 
-	//REVISAR, NO ORDENA POR FECHA
-	@Query("SELECT c FROM Curso c JOIN c.fechas f WHERE c.activo = (:activo) ORDER BY f.fecha ASC")
+	// Filtra las notas de pedido activas en el sistema
+	@Query("SELECT c FROM Curso c JOIN c.fechas f WHERE c.activo = (:activo)")
 	public List<Curso> findAll(boolean activo);
 
+	// Filtra las notas de pedido activas en el sistema y si están pendientes a asignarle espacios o no
 	@Query("SELECT c FROM Curso c JOIN c.fechas f WHERE c.activo = (:activo) AND c.pendiente = (:pendiente)")
 	public List<Curso> findAll(boolean activo, boolean pendiente);
 }
